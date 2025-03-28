0ca93c33f264c318235136393597fc5a4a379f6b
hime2828 <ramidisaisuhas@gmail.com>
2025-03-28 13:54:43 +0530

    suhas

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    <h1>Sangamithra</h1>
+     <h2>300 mathrame</h2>
+     <h3>+91 7288928789</h3>
+    </>
+  )
+}
+
+export default App
